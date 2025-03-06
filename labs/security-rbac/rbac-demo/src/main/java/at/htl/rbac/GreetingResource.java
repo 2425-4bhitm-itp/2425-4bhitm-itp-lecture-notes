5c6edea4dda16fa021aa91dbc1f54c825b753835
htl-leonding <t.stuetz@htl-leonding.ac.at>
2025-03-06 19:50:53 +0100

    lecture notes 2025-03-06 (security) part 2

--- labs/security-rbac/rbac-demo/src/main/java/at/htl/rbac/GreetingResource.java
+++ labs/security-rbac/rbac-demo/src/main/java/at/htl/rbac/GreetingResource.java
@@ -1,5 +1,7 @@
 package at.htl.rbac;
 
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,18 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @PermitAll
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @RolesAllowed("admin")
+    @GET
+    @Path("admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String adminGreeting() {
+        return "Hello Admin!";
     }
 }
